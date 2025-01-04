da7b663d2021c8d5485ba69735b6fd8d662b858d
anon <anon@anon.com>
2025-01-04 09:04:40 -0300

    nost-atualization

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 
     importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1735991182131" }, { url: "/sw.js", revision: "1735991182132" }, { url: "/manifest.webmanifest", revision: "1735991182133" }] });
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1735991204486" }, { url: "/sw.js", revision: "1735991204487" }, { url: "/manifest.webmanifest", revision: "1735991204488" }] });
   
